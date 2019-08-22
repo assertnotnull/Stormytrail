2165996e11c327b7d2cfc21f8deb5c0421d7ee66
Patrice Gauthier <patgauth@gmail.com>
2019-08-22 14:06:25 -0400

    get container names and networks

--- src/client/App.svelte
+++ src/client/App.svelte
@@ -1,12 +1,13 @@
-<div class="flex h-screen">
-	<div class="flex w-1/6 bg-gray-200 h-12">
-		{#each containerNames as name}
-		<!-- <input class="" type="checkbox" id="{name}" name="{name}">
-		<label class="text-sm" for="{name}">{name}</label> -->
-		<label><input type="checkbox" bind:group={selectContainers} value={name}>{name}</label>
+<div class="flex flex-row bg-gray-200 h-screen">
+	<div class="flex flex-col">
+	{#each networks as network}
+		<p>{network}</p>
+		{#each containerNames as container}				
+		<label><input type="checkbox" bind:group={selectContainers} value={container.id}>{container.name}</label>
 		{/each}
+	{/each}
 	</div>
-	<div class="w-5/6 bg-black text-white ">
+	<div class="w-5/6 bg-black text-white">
 		{history}
 	</div>
 </div>
@@ -17,21 +18,24 @@
 	const socket = io('http://localhost:3000')
 
 	let containerNames = []
-	let containers = []
+	let containersUp = []
+	let networks = []
 	let history = []
 	let selectContainers = []
 
 	$: {
 		socket.emit(selectContainers)
-		io.subscribe(selectContainers, data => {
-			console.log('got data in container')
-            history.push(data)
-		})
 	}
 
 	socket.on('initialize', data => {
-		containers = data.containers.filter(container => container.State === 'running')
-		containerNames = containers.map(container => container.Names[0])
+		containersUp = data.containers.filter(container => container.State === 'running')
+		containerNames = containersUp.map(container => {
+			let found = container.Names[0].match(/([a-z]+)_(\w+)_(\d+)_.*/)
+			return found ? {id: found[0], name: found[2], network: found[1]} : {id: container.Names[0], name: container.Names[0].substring(1)}
+		})
+		networks = [...new Set(containerNames.map(container => {
+			return container.network ? container.network : 'default'
+		}))]
 	})
 	socket.on('logs', data => {
 		history[data.id] += data.logs
