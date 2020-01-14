afe1ba247b1e35329147c3f8cea88ce86d9744cb
Patrice Gauthier <patgauth@gmail.com>
2020-01-14 17:31:52 -0500

    wip

--- src/server/server.js
+++ src/server/server.js
@@ -6,21 +6,33 @@ const Docker = require('dockerode')
 const docker = new Docker({socketPath: '/var/run/docker.sock'});
 const io = require('socket.io')(fastify.server);
 const Rx = require('rxjs');
-const {filter, takeUntil, finalize, merge} = require('rxjs/operators');
+const {Subject, generate, interval, timer, from} = require('rxjs');
+const {filter, takeUntil, finalize, merge, switchMap, tap} = require('rxjs/operators');
 const sparser = require('./simpleparser')
 
 let mySocket
 let Logs = {}
 let listeningContainers = {}
-
+let containers = []
 
 async function start() {
     const host = process.env.HOST || 'localhost'
     const port = process.env.PORT || 3000
 
-    let containers = await docker.listContainers({all: true})
-    containers = containers.filter(container => container.State === 'running')
-
+    // let containers = await docker.listContainers({all: true}).filter(container => container.State === 'running')    
+    // const dockerPromise = from(docker.listContainers({all: true}));
+    const source = timer(0, 2000).pipe(
+        tap(_ => console.log('start')),
+        switchMap(_ => docker.listContainers({all: true})),
+        filter(container => container.State === 'running'),
+        tap(c => console.log(c))
+        )
+    
+    source.subscribe(runningContainers => {
+        console.log(runningContainers)
+        containers = runningContainers
+    });
+    
     io.on('connection', async socket => {
         fastify.log.info('connected')
 
