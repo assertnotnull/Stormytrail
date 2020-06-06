eff68ffa0c3a394170b6bf6e30f4a97b6b20c4ff
Patrice Gauthier <patgauth@gmail.com>
2020-06-05 22:02:30 -0400

    remove docker files

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM node:12-alpine
-COPY echo.js /
-CMD ["node", "echo.js"]
--- echo.js
+++
@@ -1,4 +0,0 @@
-const start = process.hrtime.bigint();
-setInterval(() => {
-    console.log(`This is an echo message ${process.hrtime.bigint() - start}`)
-}, 1000)
